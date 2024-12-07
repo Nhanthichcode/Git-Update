6057a63b1028a47c28e652516b5eb99f82bba0bf
trinhan0603 <nhanlx154@gmail.com>
2024-12-07 23:48:49 +0700

    done

--- script.js
+++ script.js
@@ -16,25 +16,3 @@ const btn_maytinh = document.getElementById("may_tinh");
 btn_maytinh.addEventListener("click", () => {
   window.location.href = "maytinhdongian.html";
 });
-
-function GiaiThua(n) {
-  if (n <= 0) return 1;
-  else {
-    return GiaiThua(n - 1) * n;
-  }
-}
-const btn_tinh_giaithua = document.getElementById("btn_tinh_giaithua");
-btn_tinh_giaithua.addEventListener("click", () => {
-  let number_inputt = document.getElementById("txt_soN");
-  let number = parseInt(number_inputt.value);
-  let txt_result = document.getElementById("txt_result_giaithua");
-  txt_result.textContent = " " + GiaiThua(number);
-});
-
-const btn_reset = document.getElementById("btn_reset");
-btn_reset.addEventListener("click", () => {
-  const txt_input = document.getElementById("txt_soN");
-  const txt_result1 = document.getElementById("txt_result_giaithua");
-  txt_input.value = 0;
-  txt_result1.value = 0;
-});
---
+++ script_2.js
@@ -0,0 +1,24 @@
+function GiaiThua(n) {
+  if (n <= 0) {
+    console.log("Nhập lại");
+
+    return 1;
+  } else {
+    return n * GiaiThua(n - 1);
+  }
+}
+const btn_tinh = document.getElementById("btn_tinh_giaithua");
+btn_tinh.addEventListener("click", () => {
+  let number_inputt = document.getElementById("txt_input");
+  let number = parseInt(number_inputt.value);
+  let txt_result = document.getElementById("txt_result");
+  txt_result.textContent = "Giai thừa của " + number + " = " + GiaiThua(number);
+});
+
+const btn_reset = document.getElementById("btn_reset");
+btn_reset.addEventListener("click", () => {
+  const txt_input = document.getElementById("txt_input");
+  const txt_result1 = document.getElementById("txt_result");
+  txt_input.value = " ";
+  txt_result1.textContent = "Nhập 1 số nguyên dương khác ";
+});
